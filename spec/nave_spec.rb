6b63dbcd685f82e2af3c0fc4adf623eb12fc34a6
gvquiroz <gvquiroz@gmail.com>
2016-06-25 15:47:16 -0300

    Agregadas pruebas de choques en nave

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../model/nave_espacial'
+require_relative '../model/misil_espacial'
 class NaveSpec
   describe 'Nave' do
     it 'should be not null' do
@@ -7,6 +8,7 @@ class NaveSpec
       expect(mi_nave).to be_truthy
     end
 
+=begin
     it 'masa should be 50' do
       mi_nave = NaveEspacial.new(100,50)
       expect(mi_nave.masa).to eq(50)
@@ -21,6 +23,7 @@ class NaveSpec
       mi_nave = NaveEspacial.new(100,50)
       expect(mi_nave.masa).to eq(50)
     end
+=end
 
     it 'vida should be 90 as efecto destructivo takes 10 life units' do
       mi_nave = NaveEspacial.new(100,50)
@@ -43,6 +46,29 @@ class NaveSpec
       mi_nave = NaveEspacial.new(100,50)
       mi_nave.efecto_masa(-10)
       expect(mi_nave.masa).to eq_to(40)
+    end
+
+    it 'vida should be 0 as the consequence of colision with NaveEspacial efecto destructivo 100' do
+
+      reglas_de_colision_nave = { NaveEspacial => lambda { |x| x.efecto_destructivo(100) }
+      }
+
+      mi_nave = NaveEspacial.new(100,50)
+      mi_nave.reglas = reglas_de_colision_nave
+      mi_nave.resolver_choque_con(NaveEspacial.new(100,50))
+      expect(mi_nave.vida).to be(0)
+
+    end
+    it 'vida should be 0 as the consequence of colision with MisilEspacial efecto destructivo 80' do
+
+      reglas_de_colision_nave = { MisilEspacial => lambda { |x| x.efecto_destructivo(80) }
+      }
+
+      mi_nave = NaveEspacial.new(100,50)
+      mi_nave.reglas = reglas_de_colision_nave
+      mi_nave.resolver_choque_con(MisilEspacial.new(100,50))
+      expect(mi_nave.vida).to be(20)
+
     end
 
   end
