7d93d83515b832c10d8298b77a79506c6f3f9af2
gvquiroz <gvquiroz@gmail.com>
2016-06-27 14:34:20 -0300

    refactor y agregado pruebas a elemento espacial spec

--- spec/elemento_espacial_spec.rb
+++ spec/elemento_espacial_spec.rb
@@ -4,44 +4,64 @@ require_relative '../model/nave_espacial'
 
 
 class ElementoEspacialSpec
-describe  do
-  it 'should be not null' do
-    reglas_de_colision_nave = nil
-    reglas = { NaveEspacial.class => reglas_de_colision_nave}
+describe 'Elemento Espacial como Nave' do
+  it 'la instancia de la nave deberia no ser falso ni nil' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
     mi_nave = NaveEspacial.new(100, 50,reglas)
+
     expect(mi_nave).to be_truthy
   end
 
-  it 'vida should be 90 as efecto destructivo takes 10 life units' do
-    reglas_de_colision_nave = nil
-    reglas = { NaveEspacial.class => reglas_de_colision_nave}
+  it 'vida deberia ser 90 ya que efecto destructivo le quita 10 unidades de vida' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
     mi_nave = NaveEspacial.new(100, 50,reglas)
+
     mi_nave.efecto_destructivo(10)
+
     expect(mi_nave.vida).to eq_to(90)
   end
-  it 'vida should be 110 as efecto constructivo gives 10 life units' do
-    reglas_de_colision_nave = nil
-    reglas = { NaveEspacial.class => reglas_de_colision_nave}
+  it 'vida deberia ser 110 ya que efecto constructivo le da 10 unidades de vida' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
     mi_nave = NaveEspacial.new(100, 50,reglas)
+
     mi_nave.efecto_constructivo(10)
+
     expect(mi_nave.vida).to eq_to(110)
   end
 
-  it 'masa should be 60 as efecto masa gives 10 masa units' do
-    reglas_de_colision_nave = nil
-    reglas = { NaveEspacial.class => reglas_de_colision_nave}
+  it 'masa deberia ser 60 ya que efecto masa le da 10 unidades de masa' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
     mi_nave = NaveEspacial.new(100, 50,reglas)
+
     mi_nave.efecto_masa(10)
+
     expect(mi_nave.masa).to eq_to(60)
   end
 
-  it 'masa should be 40 as efecto masa gives -10 masa units' do
-    reglas_de_colision_nave = nil
-    reglas = { NaveEspacial.class => reglas_de_colision_nave}
+  it 'masa deberia ser 40 ya que efecto masa le quita 10 unidades de masa' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
     mi_nave = NaveEspacial.new(100, 50,reglas)
+
     mi_nave.efecto_masa(-10)
+
     expect(mi_nave.masa).to eq_to(40)
   end
+
+  it 'el estado de la nave deberia ser muerto al decirle destruccion total' do
+    reglas_de_colision_nave = { NaveEspacial => nil }
+    reglas = { NaveEspacial => reglas_de_colision_nave}
+    mi_nave = NaveEspacial.new(100, 50,reglas)
+
+    mi_nave.destruccion_total
+
+    expect(mi_nave.esta_vivo).to be false
+  end
+
 end
 
 end
