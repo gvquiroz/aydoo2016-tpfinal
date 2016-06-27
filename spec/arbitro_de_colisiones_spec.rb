a36c74137c0aaa3b541ccf064da9f17366ad0cc3
gvquiroz <gvquiroz@gmail.com>
2016-06-27 16:46:58 -0300

    agregados nuevos test de integracion

--- spec/arbitro_de_colisiones_spec.rb
+++ spec/arbitro_de_colisiones_spec.rb
@@ -58,4 +58,43 @@ describe 'ArbitroDeColisiones' do
 
   end
 
+  it 'un misil deberia tener masa 50 al chocarse con otro misil que disminuye su vida en 100' do
+
+    reglas = ReglasDeObjetosEspaciales.new
+    arbitro = ArbitroDeColisiones.new
+
+    un_misil = MisilEspacial.new(150,50,reglas.reglas_de_colision)
+    otro_misil = MisilEspacial.new(100,150,reglas.reglas_de_colision)
+
+    arbitro.resolver_colision(un_misil,otro_misil)
+    expect(un_misil.vida).to eq(50)
+
+  end
+
+  it 'una bomba deberia tener masa 50 al chocarse con otro misil que disminuye su vida en 100' do
+
+    reglas = ReglasDeObjetosEspaciales.new
+    arbitro = ArbitroDeColisiones.new
+
+    una_nave = NaveEspacial.new(150,50,reglas.reglas_de_colision)
+    una_bomba = BombaEspacial.new(150,150,reglas.reglas_de_colision)
+
+    arbitro.resolver_colision(una_nave,una_bomba)
+    expect(una_bomba.vida).to eq(50)
+
+  end
+
+  it 'una estrella deberia tener estar muerta al chocarse con una bomba por destruccion total' do
+
+    reglas = ReglasDeObjetosEspaciales.new
+    arbitro = ArbitroDeColisiones.new
+
+    una_estrella = EstrellaEspacial.new(150,50,reglas.reglas_de_colision)
+    una_bomba = BombaEspacial.new(150,150,reglas.reglas_de_colision)
+
+    arbitro.resolver_colision(una_estrella,una_bomba)
+    expect(una_estrella.esta_vivo).to be false
+
+  end
+
 end
